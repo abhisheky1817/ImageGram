142cd424f122fdf607f1339ecfb0edca07b1543f
abhisheky1817 <abhisheky.1817@gmail.com>
2026-02-09 22:19:22 +0530

    added userSchema

---
+++ src/schema/userschema.js
@@ -0,0 +1,38 @@
+import moongose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 5
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 6,
+        validate: {
+            validator: function (emailValue) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailValue);
+            },
+            message: 'Invalid email format'
+        },
+        
+    },
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "admin"]
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5
+    }
+}, { timestamps: true });
+
+
+const user = mongoose.model("User", userSchema); // user collection
+
+export default user;
