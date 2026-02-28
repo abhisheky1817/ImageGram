b776ff0320b41c3d3582a7d1c095339b6dbafc05
abhisheky1817 <abhisheky.1817@gmail.com>
2026-03-01 00:34:08 +0530

    added middleware demo

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { createPost } from "./controllers/postControllers.js";
 
 const app = express();
 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.text()); /
 
 app.get("/", (req, res) => {
   res.send("Welcome to ImageGram!");
