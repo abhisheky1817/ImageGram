a03e31afc9e831710afcac4d992f7d6252682fde
abhisheky1817 <abhisheky.1817@gmail.com>
2026-03-03 10:03:45 +0530

    added validation layer(zod)

--- src/routers/v1/post.js
+++ src/routers/v1/post.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { createPost } from "./controllers/postControllers.js";
 import upload from "./config/multerconfig.js";
 import { deletePost, updatePost } from '../../controllers/postControllers.js';
+import { validate } from '../../validators/zodValidators.js';
+import { zodPostSchema } from '../../validators/zodPostSchema.js';
 
 const router = express.Router();  //router object to modularize routes
 
 
-router.post('/', upload.single('image'), createPost);
+router.post('/',validate(zodPostSchema), upload.single('image'), createPost);
 
 router.delete('/:id', deletePost);
 
