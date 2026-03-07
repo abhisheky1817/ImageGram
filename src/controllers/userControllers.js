6f395a80f4a37fabe7727504e66fc8f8774c2282
abhisheky1817 <abhisheky.1817@gmail.com>
2026-03-07 07:53:17 +0530

    Added signin

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,4 @@
-import { signupUserService } from "../services/userServicea.js";
+import { signupUserService, signinUserService  } from "../services/userServicea.js";
 
 export async function signup(req, res) {
     try {
@@ -9,6 +9,29 @@ export async function signup(req, res) {
             data: user
         });
     } catch (error) {
+        console.log(error);
+        if(error.status) {
+            return res.status(error.status).json({
+                success: false,
+                message: error.message
+            })
+        }
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
+
+export async function signin(req, res) {
+    try {
+        const response = await signinUserService(req.body);
+        return res.status(200).json({
+            success: true,
+            message: "User signed in successfully",
+            data: response
+        });
+    } catch(error) {
         console.log(error);
         if(error.status) {
             return res.status(error.status).json({
--- src/routers/v1/user.js
+++ src/routers/v1/user.js
@@ -3,7 +3,12 @@ import express from "express";
 import { signup } from "./controllers/userControllers.js";
 import { validate } from "../../validators/zodValidators.js";
 import { zodSignupSchema } from "../../validators/zodSignupSchema.js";
+import { zodSigninSchema } from "../../validators/zodSigninSchema.js";
 
 const router = express.Router();
 
 router.post('/signup', validate(zodSignupSchema), signup);
+
+router.post('/signin', validate(zodSigninSchema), signin);
+
+export default router;
