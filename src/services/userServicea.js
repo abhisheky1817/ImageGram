76d55df59690cd55c95a919ac382517d2c6cad63
abhisheky1817 <abhisheky.1817@gmail.com>
2026-03-09 22:50:29 +0530

    Added signin aut

--- src/services/userServicea.js
+++ src/services/userServicea.js
@@ -48,3 +48,12 @@ export const signinUserService = async (userDetails) => {
         throw error;
     }
 }
+
+export const checkIfUserExists = async (email) => {
+    try {
+        const user = await findUserByEmail(email);
+        return user;
+    } catch (error) {
+        throw error;
+    }
+}
