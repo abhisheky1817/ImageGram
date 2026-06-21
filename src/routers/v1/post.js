f44259ed48def681739ab40d1ff1f4405b84b91f
abhisheky1817 <abhisheky.1817@gmail.com>
2026-06-21 11:54:29 +0530

    zod validator

--- src/routers/v1/post.js
+++ src/routers/v1/post.js
@@ -2,18 +2,20 @@ import express from 'express';
 //import { createPost } from "./controllers/postControllers.js";
 import { createPostController, getAllPosts, deletePost } from '../../controllers/postControllers.js';
 import { upload } from "../../config/multerconfig.js";
+import { validate } from '../../validators/zodValidator.js';
+import { zodPostSchema } from '../../validators/zodPostSchema.js';
 
 
 const router = express.Router();  //router object to modularize routes
 
 
-router.post('/', upload.single('image'), createPostController);  //if the remaining path after /api/v1/posts is /, then the request will be forwarded to createPostController
+router.post('/', upload.single('image'), validate(zodPostSchema), createPostController);  //if the remaining path after /api/v1/posts is /, then the request will be forwarded to createPostController
 
 router.get('/', getAllPosts);  //if the remaining path after /api/v1/posts is /, then the request will be forwarded to getAllPostsController
 
 router.delete('/:id', deletePost);  //if the remaining path after /api/v1/posts is /:id, then the request will be forwarded to deletePostController
 
-router.put('/:id', upload.single('image'), updatePostController);
+router.put('/:id', upload.single('image'), validate(zodPostSchema), updatePost);
 
 
 export default router;
